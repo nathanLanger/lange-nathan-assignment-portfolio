b02f12a65a95d364b50c46927cecc794e9592b50
unknown <nlange2021@gmail.com>
2023-02-06 04:58:00 -0600

    Added all the files.

---
+++ static/index.js
@@ -0,0 +1,11 @@
+/*
+Author: Nathan Lange
+Class:  Software Engineering
+Description: Assignment Portfolio
+*/
+function dispWebDiceRoller() {
+    document.getElementById("frame").src = "https://lange-nathan-web-dice-roller.azurewebsites.net/";
+}
+function dispWebResume() {
+    document.getElementById("frame").src = "https://lange-nathan-web-resume.azurewebsites.net/";
+}

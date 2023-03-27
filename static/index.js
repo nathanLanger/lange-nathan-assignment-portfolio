5fce7a6c1a8b0ee7bcc27d92030cfa1deac92305
nathanLanger <112519430+nathanLanger@users.noreply.github.com>
2023-03-26 21:55:44 -0500

    added more sites

--- static/index.js
+++ static/index.js
@@ -9,3 +9,15 @@ function dispWebDiceRoller() {
 function dispWebResume() {
     document.getElementById("frame").src = "https://lange-nathan-web-resume.azurewebsites.net/";
 }
+function dispGettingToKnowEachOtherTeams() {
+    document.getElementById("frame").src = "https://polite-glacier-0ddda8210.2.azurestaticapps.net/";
+}
+function dispHealthRiskCalculator() {
+    document.getElementById("frame").src = "https://lively-stone-0c2b84410.2.azurestaticapps.net/";
+}
+function dispFinalProjectProposal() {
+    document.getElementById("frame").src = "https://mango-meadow-018398610.2.azurestaticapps.net/";
+}
+function dispECAMSStatic() {
+    document.getElementById("frame").src = "https://lively-water-0a6447110.2.azurestaticapps.net/";
+}
